729652743ec390d514f05ff5a0869a2f92a25046
Friedrich Eggers <fritz_eggers@gmx.de>
2026-03-09 17:24:50 +0100

    added texlive-fontsrecommended

--- install-texlive.sh
+++ install-texlive.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install texlive and pandoc
-yay -S --noconfirm --needed texlive-xetex texlive-fontsextra
+yay -S --noconfirm --needed texlive-xetex texlive-fontsextra texlive-fontsrecommended
