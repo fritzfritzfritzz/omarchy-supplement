e4dcb1e98381413df2e98b9fd35098fc9512f0a4
Friedrich Eggers <fritz_eggers@gmx.de>
2026-08-06 12:54:06 +0200

    added bash to install-dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -25,10 +25,11 @@ fi
 # Check if the clone was successful
 if [ $? -eq 0 ]; then
   echo "removing old configs"
-  rm -rf ~/.config/nvim ~/.local/share/nvim/ ~/.cache/nvim/ ~/.config/ghostty/config # ~/.config/starship.toml
+  rm -rf ~/.config/nvim ~/.local/share/nvim/ ~/.cache/nvim/ ~/.config/ghostty/config ~/.bashrc # ~/.config/starship.toml
 
   cd "$REPO_NAME"
   # stow zshrc
+  stow bash
   stow ghostty
   # stow tmux
   stow nvim
